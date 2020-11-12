bda3a1eb3f7e8375e3b7b5aa843949faed2472b1
nadia <satsisat@yahoo.fr>
2020-11-13 00:17:52 +0100

    initialize classes

---
+++ __main__.py
@@ -0,0 +1,10 @@
+import sys
+from game import Game
+
+def main(args=None):
+  game = Game()
+  game.play()
+
+if __name__ == "__main__":
+  sys.exit(main(sys.argv[1:]))
+
---
+++ board.py
@@ -0,0 +1,32 @@
+import string
+import random
+from card import Card
+
+
+class Board:
+    size = 6
+
+    def __init__(self):
+        pairCount = int(self.size * self.size / 2)
+        values = list(string.ascii_uppercase[0:pairCount])
+        values += values  # double les valeurs
+        random.shuffle(values)
+        self.cards = [Card(value) for value in values]
+
+    def showCard(self, i):
+        self.cards[i].isShown = True
+
+    def hideCard(self, i):
+        self.cards[i].isShown = False
+
+    def draw(self):
+        display = ""
+        for i, card in enumerate(self.cards):
+            if card.isShown:
+                display += " {} ".format(card.value)
+            else:
+                display += " X "
+            if i % self.size == self.size - 1 and i != self.size * self.size - 1:
+                display += "\n"
+        print(display)
+
