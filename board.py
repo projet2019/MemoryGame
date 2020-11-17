a829b3a568b217bce3a46bae8336c37f4d379798
joel yepgang <joelyepgang2019@yahoo.com>
2020-11-17 06:55:01 +0100

    add game mecanic

---
+++ board.py
@@ -0,0 +1,38 @@
+import string
+import random
+from card import Card
+
+
+class Board:
+    size = 6
+    size = 4
+
+    def __init__(self):
+        pairCount = int(self.size * self.size / 2)
+        values += values  # double les valeurs
+        random.shuffle(values)
+        self.cards = [Card(value) for value in values]
+
+    def getCard(self, i):
+        return self.cards[i].value
+
+    def showCard(self, i):
+        self.cards[i].isShown = True
+
+    def hideCard(self, i):
+        self.cards[i].isShown = False
+
+    def isOnBoard(self, i):
+        return i >= 0 and i < self.size * self.size
+
+    def draw(self):
+        display = ""
+        for i, card in enumerate(self.cards):
+            if card.isShown:
+                display += " {} ".format(card.value)
+            else:
+                display += " X "
+            if i % self.size == self.size - 1 and i != self.size * self.size - 1:
+                display += "\n"
+        print(display)
+
