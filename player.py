3c2fe65586f1dd172a1f3b0ff29e6427b44f5f4d
joel yepgang <joelyepgang2019@yahoo.com>
2020-11-17 07:37:15 +0100

    add game mecanic

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    score = 0;
+
+    def __init__(self):
+        self.cards = set()
+
+    def gainCard(self, card):
+        self.cards.add(card)
+
+    def getScore(self):
+        return len(self.cards)
