73757c0c45c3e33c91763a4d7a798c7a83509e4b
joel yepgang <joelyepgang2019@yahoo.com>
2020-12-13 12:48:33 +0100

    add unit testing isOnBoard and isNotOnBoard

--- test.py
+++ test.py
@@ -9,3 +9,14 @@ def testHideCard(self):
     self.assertFalse(self.board.isShown(3), "La carte devrait être cachée")
 
 
+def test5IsOnBoard(self):
+    """Test qu'un nombre est bien dans les limites du plateau"""
+    self.assertTrue(self.board.isOnBoard(5), "5 devrait faire partie du plateau de jeu")
+
+
+def test18IsNotOnBoard(self):
+    """Test qu'un nombre est bien en dehors des limites du plateau"""
+    self.assertFalse(self.board.isOnBoard(18), "18 ne devrait pas faire partie du plateau de jeu")
+
+
+
